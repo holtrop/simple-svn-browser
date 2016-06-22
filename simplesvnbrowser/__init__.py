610d69156ed9057167340dd22034bbdc5d5edd9e
Josh Holtrop <jholtrop@gmail.com>
2016-06-22 14:26:12 -0400

    add run_svn()

--- simplesvnbrowser/__init__.py
+++ simplesvnbrowser/__init__.py
@@ -1,1 +1,9 @@
 from simplesvnbrowser.main_window import MainWindow
+import subprocess
+
+def run_svn(args):
+    completed_process = subprocess.run(
+            ["svn", "--non-interactive"] + args,
+            stdout = subprocess.PIPE,
+            stderr = subprocess.PIPE)
+    return completed_process.stdout.decode(), completed_process.stderr.decode()
