725e241984a663df90bfd49d1444b506d83fe5b3
Josh Holtrop <jholtrop@gmail.com>
2016-06-22 14:26:47 -0400

    determine URL to start with in cli.run()

--- simplesvnbrowser/cli.py
+++ simplesvnbrowser/cli.py
@@ -1,4 +1,21 @@
 import simplesvnbrowser
+import re
 
 def run(argv):
-    print(argv)
+    url = None
+    wc_path = "."
+    if len(argv) >= 2:
+        if re.search(r'://', argv[1]):
+            url = argv[1]
+        else:
+            wc_path = argv[1]
+    if url is None:
+        url = determine_url_from_wc(wc_path)
+    # TODO: start with url
+
+def determine_url_from_wc(path):
+    stdout, _ = simplesvnbrowser.run_svn(["info", path])
+    m = re.search(r'^URL: (.*)$', stdout, re.MULTILINE)
+    if m is not None:
+        return m.group(1)
+    return ""
