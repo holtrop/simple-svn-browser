ff430fd1275a3e7b890e69fd3a0395a9ed305571
Josh Holtrop <jholtrop@gmail.com>
2016-06-22 17:03:21 -0400

    implement CacheFile a bit more

--- simplesvnbrowser/cache_file.py
+++ simplesvnbrowser/cache_file.py
@@ -12,6 +12,19 @@ class CacheFile:
                 with open(cfp, "r") as f:
                     cache_content = f.read()
                 exec(cache_content, CacheFile.params)
+                del(CacheFile.params["__builtins__"])
+
+    def __getitem__(self, key):
+        return CacheFile.params.get(key, None)
+
+    def __setitem__(self, key, value):
+        CacheFile.params[key] = value
+
+    def write(self):
+        cfp = self.__cache_file_path()
+        with open(cfp, "w") as f:
+            for k, v in CacheFile.params.items():
+                f.write("%s = %s\n" % (k, repr(v)))
 
     def __cache_dir(self):
         return xdg.BaseDirectory.save_cache_path("simple-svn-browser")
